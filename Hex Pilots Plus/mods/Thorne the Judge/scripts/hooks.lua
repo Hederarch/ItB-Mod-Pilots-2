44766aa6e713687c7cdaec42040c1d15d3a4c658
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-05-08 19:22:27 +1000

    Exclude all Bots

--- Hex Pilots Plus/mods/Thorne the Judge/scripts/hooks.lua
+++ Hex Pilots Plus/mods/Thorne the Judge/scripts/hooks.lua
@@ -38,14 +38,13 @@ local function Hedera_Sunrise(pawnId)
 		for i = 1, #pawnList do
 			local unit = Board:GetPawn(pawnList[i])
 			local space = unit:GetSpace()
-			if unit:GetTeam() ~= TEAM_PLAYER and Board:IsValid(space) then
-				if (GetCurrentMission().ID == "Mission_FreezeBots" and unit:GetDefaultFaction() ~= FACTION_BOTS) or GetCurrentMission().ID ~= "Mission_FreezeBots" then--if you don't think this is appropriate for whatever balance reasons remove the condition, but I personally think that it should not screw you over on this mission (same line of reasoning as Void Shocker). If you want to go all the way, then don't check for the mission, since it's a massive advantage to the player to insta-kill Bots, imho. But YMMV.
-					modApi:scheduleHook(100*i, function()
-						if Game then Game:TriggerSound("/props/fire_damage") end
-						if Board then Board:AddAnimation(space,"ExploSunbeam",0) Board:SetFire(space,true) end
-					end)--100ms between each one seems to be good, but adjust it as you see fit. If you want the first one to be delayed differently, replace it with this exact expression: (i>1 and 100*i) or first_delay
-					--where first_delay is whatever value in ms you want it to be.
-				end
+			if unit:GetTeam() ~= TEAM_PLAYER and Board:IsValid(space) and unit:GetDefaultFaction() ~= FACTION_BOTS then
+			--Your description says Add Fire to all Vek, which excludes Bots. Also better since Boom Bots are a thing, and would cause you to instantly fail the Freeze and Defend Robots Mission.
+				modApi:scheduleHook((i>1 and 100*i) or 0, function()
+					if Game then Game:TriggerSound("/props/fire_damage") end
+					if Board then Board:AddAnimation(space,"ExploSunbeam",0) Board:SetFire(space,true) end
+				end)--100ms between each one seems to be good, but adjust it as you see fit. If you want the first one to be delayed differently, replace it with this exact expression: (i>1 and 100*i) or first_delay
+				--where first_delay is whatever value in ms you want it to be.
 			end
 		end
 	end
